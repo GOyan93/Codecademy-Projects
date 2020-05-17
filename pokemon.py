7b2d27e6aafac108202a4b87fe0fe0ebb5a2ea8f
Greg <goyandle@gmail.com>
2020-05-16 20:05:02 -0400

    Added TODO list. Added test loop. Added potion and revive counters.

--- pokemon.py
+++ pokemon.py
@@ -9,7 +9,22 @@ Description: This is a program that uses OOP to simulate the basic battle struct
 
 
 """
-import random
+
+# TODO Create main game structure
+  # TODO Ask player for name (istantiate Trainers)
+  # TODO Display Pokemon List
+  # TODO Players choose pokemon (adds to pkm ball list)
+    # TODO removes Pokemon from list OR instantiates Pokemon
+  # TODO Choose pokeballs to create active pokemons
+  # TODO While loop based off turn number (odd = player 1, even = player 2)
+    # TODO Player chooses actions
+  # TODO End Game when player has no active pokemon in pokeball list
+# TODO Fix revive function
+
+
+
+
+import random, sys, time
 
 
   
@@ -56,7 +71,6 @@ class Pokemon:
       print(f'{self.name} has attacked {other_pkm.name}!')
       attack_power = int(random.randint(self.attack_range[0], self.attack_range[1]) * self.type_mult(other_pkm))
       other_pkm.lose_health(attack_power)
-    
 
   def display(self):
     print(f'{self.name}, Health: {self.current_hp}')
@@ -67,58 +81,75 @@ class Trainer:
     self.name = name 
     self.pkm_balls = []
     self.num_of_pkballs = len(self.pkm_balls)
-    self.active_pkm = active_pkm # 1-6
-
+    self.active_pkm = None
+    self.potions = 3
+    self.revives = 1
+    
   def add_pkm(self, pkm):
-    if self.num_of_pkballs < 3:
+    if self.num_of_pkballs < 1:
       self.pkm_balls.append(pkm)
       print(f'{pkm.name} has been added to your collection')
     else:
       print("You cannot add anymore pokemon!")
-      
-    else:
-      print('You already have 6 pokemon. You cannot add any more.')
     
-  def use_potion(self, pkm):
-    pkm.gain_health(15)
-    print(f'You have given {pkm.name} a potion!')
+  def use_potion(self):
+    if self.potions > 0:
+      print(f'You have given {pkm.name} a potion!')
+      self.active_pkm.gain_health(15)
+      self.potions -= 1
+      Turn += 1 
+    print('You do not have any potions left')
+
+  def use_revive(self, pkm):
+    if self.revives > 0 and pkm.current_health <= 0:
+      self.pkm.gain_health(self.pkm.max_health * 0.5)
+      self.revives -= 1
+      print(f'You have revived {pkm.name}!')
+    print('You do not have any revives left')
 
   def attack(self, other_pkm):
     self.active_pkm.attack(other_pkm)
-    # This needs some editing?
-    pass
 
-  def switch_pkm(self, other_pkm):
-    choice = int("Choose your pokemon (1-3):/n")
-    self.active_pk = self.pkm_balls[choice+1]
-    print(f"{self.active_pk.name} I choose you!")
+  def switch_pkm(self):
+    for pkm in self.pkm_balls:
+      if pkm.has_fainted == False:
+    choice = input("Choose your pokemon (1-3):\n")
+    self.active_pkm = self.pkm_balls[int(choice)-1]
+    print(f"{self.active_pkm.name} I choose you!")
+    
 
   def run_away(self):
-    trainer_input = input("Are you sure you want to run? (Yes or No)")
+    trainer_input = input("Are you sure you want to run? (Yes or No)\n")
     if 'y' in trainer_input.lower():
       print('You have successfully run away')
+      sys.exit()
       #replay function
     else:
       return None
     
-
+  def display_pokeballs(self):
+    #displays list of pokemon if 
 
 # Pokemon Instances
-bulbasaur = Pokemon('Bulbasaur', random.randint(1, 10), 'grass', 50, (5, 10))
-charmander = Pokemon('Charmander', random.randint(1, 10), 'fire', 30, (9, 15))
-squirtle = Pokemon('Squirtle', random.randint(1, 10), 'water', 40, (7, 12))
+bulbasaur = Pokemon('Bulbasaur', random.randint(5, 10), 'grass', 50, (5, 10))
+charmander = Pokemon('Charmander', random.randint(5, 10), 'fire', 30, (9, 15))
+squirtle = Pokemon('Squirtle', random.randint(5, 10), 'water', 40, (7, 12))
+
+pokemon_list = [bulbasaur, charmander, squirtle]
 
 
 
 # Main Game Loop
+
+
 bulbasaur.display()
-charmander.display()
-while bulbasaur.has_fainted == False and charmander.has_fainted == False:
-  bulbasaur.attack(charmander)
-  charmander.attack(bulbasaur)
+squirtle.display()
+while bulbasaur.has_fainted == False and squirtle.has_fainted == False:
+  bulbasaur.attack(squirtle)
+  squirtle.attack(bulbasaur)
  
 
 print(bulbasaur)
-print(charmander)
+print(squirtle)
           
 
